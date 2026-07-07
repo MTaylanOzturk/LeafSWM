2400130b82448a982646b45f59c4c3a3f78c6d7b
MahmutTaylanOzturk <ozturkmahmuttaylan@gmail.com>
2026-07-08 01:15:26 +0300

    Add Entity import to GotoCmd for improved command functionality

--- plugin/src/main/java/com/infernalsuite/asp/plugin/commands/sub/GotoCmd.java
+++ plugin/src/main/java/com/infernalsuite/asp/plugin/commands/sub/GotoCmd.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
